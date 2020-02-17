8cc9db34a037bfb447138e93055067dbcde9f4eb
shashi-banger <shashidhar@amagi.com>
2020-02-17 10:23:44 +0530

    swig build added

--- apps/build_python_wheel.sh
+++ apps/build_python_wheel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 touch ffmpeg_lib.i
+rm ffmpeg_lib.py
+swig -python -modern -I./ts_muxer/src/ -o ffmpeg_lib_wrap.c ffmpeg_lib.i
 pip uninstall -y --exists-action w ffmpeg_lib
 python setup.py bdist_wheel --universal
 pip install dist/ffmpeg_lib-1.0.0-cp36-cp36m-linux_x86_64.whl
